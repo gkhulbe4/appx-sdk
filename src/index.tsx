5961968456cbfad60061907581399b69be55bd59
gkhulbe4 <khulbegarvit4@gmail.com>
2025-10-01 15:30:14 +0530

    changed types export

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+// JS/React
 export { AppxSdk } from "./node/index";
 export { AppxProvider } from "./react/AppxProvider";
 export { useAppx } from "./react/useAppx";
@@ -5,7 +6,5 @@ export * from "./react/hooks/useNewCourses";
 export * from "./react/hooks/usePurchases";
 
 export type { AppxContextType, CurrentUser } from "./types/appxTypes";
-
-export * from "../src/types/appxTypes";
-export * from "../src/types/coursesTypes";
-export * from "../src/types/userTypes";
+export type * from "./types/coursesTypes";
+export type * from "./types/userTypes";
