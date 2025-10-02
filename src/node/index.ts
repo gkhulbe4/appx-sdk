116eee8a83870d36e8c30998d2b8577fabc46954
gkhulbe4 <khulbegarvit4@gmail.com>
2025-10-03 05:25:03 +0530

    minor change

--- src/node/index.ts
+++ src/node/index.ts
@@ -2,14 +2,17 @@ import { createClient } from "./client";
 import { UserApi } from "./user";
 import { CoursesApi } from "./courses";
 import type { AppxSdkOptions } from "../types/appxTypes";
+import { VideoApi } from "./video";
 
 export class AppxSdk {
   public user: UserApi;
   public courses: CoursesApi;
+  public video: VideoApi;
 
   constructor(options: AppxSdkOptions) {
     const client = createClient(options.baseUrl, options.getToken);
     this.user = new UserApi(client);
     this.courses = new CoursesApi(client);
+    this.video = new VideoApi(client);
   }
 }
