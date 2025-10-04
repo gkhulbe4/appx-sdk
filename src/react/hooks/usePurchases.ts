f4fc8d404396af64ac37029f28cbe999443eb5e1
gkhulbe4 <khulbegarvit4@gmail.com>
2025-10-04 23:01:51 +0530

    added: react query for purchases

--- src/react/hooks/usePurchases.ts
+++ src/react/hooks/usePurchases.ts
@@ -1,6 +1,10 @@
 import { useState, useEffect } from "react";
 import { useAppx } from "../useAppx";
-import type { UserPurchasesResponse } from "../../types/coursesTypes";
+import type {
+  PurchasedCourse,
+  UserPurchasesResponse,
+} from "../../types/coursesTypes";
+import { useQuery } from "@tanstack/react-query";
 
 type UsePurchasesProps = {
   userId: string;
@@ -9,33 +13,15 @@ type UsePurchasesProps = {
 
 export function usePurchases({ userId, itemType }: UsePurchasesProps) {
   const { sdk } = useAppx();
-  const [purchases, setPurchases] = useState<UserPurchasesResponse>();
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    let isMounted = true;
-
-    const fetchPurchases = async () => {
-      setLoading(true);
-      setError(null);
-      try {
-        const data = await sdk.courses.getPurchasedCourses(userId, itemType);
-        if (isMounted) setPurchases(data ?? []);
-      } catch (error) {
-        console.log("Failed to fetch purchased courses:", error);
-        if (isMounted) setError((error as Error).message);
-      } finally {
-        if (isMounted) setLoading(false);
-      }
-    };
-
-    fetchPurchases();
-
-    return () => {
-      isMounted = false;
-    };
-  }, [sdk, userId, itemType]);
-
-  return { purchases, loading, error };
+  return useQuery<PurchasedCourse[], Error>({
+    queryKey: ["purchases", userId, itemType],
+    queryFn: async () => {
+      if (!sdk) throw new Error("SDK not initialized");
+      const res = await sdk.courses.getPurchasedCourses(userId, itemType);
+      return res.data ?? [];
+    },
+    staleTime: 5 * 60 * 1000,
+    retry: 1,
+    enabled: !!sdk && !!userId && !!itemType,
+  });
 }
