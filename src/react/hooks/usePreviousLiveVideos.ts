23a89bc04ce995d5ae3925f77bf4434423fd065d
gkhulbe4 <khulbegarvit4@gmail.com>
2025-10-05 22:47:44 +0530

    added: previous live videos hook

---
+++ src/react/hooks/usePreviousLiveVideos.ts
@@ -0,0 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+import { useAppx } from "../useAppx";
+import { VideoDetails } from "../../types/videoTypes";
+
+export default function usePreviousLiveVideos({
+  courseId,
+  start,
+  folderWiseCourse,
+  userId,
+}: {
+  courseId: string;
+  start: string;
+  folderWiseCourse: string;
+  userId: string;
+}) {
+  const { sdk } = useAppx();
+
+  return useQuery<VideoDetails[], Error>({
+    queryKey: ["previousLiveVideos", courseId, start, folderWiseCourse, userId],
+    queryFn: async () => {
+      if (!sdk) throw new Error("SDK not initialized");
+
+      const res = await sdk.video.getPreviousLiveVideos(
+        courseId,
+        start,
+        folderWiseCourse,
+        userId
+      );
+      return res.data ?? [];
+    },
+    staleTime: 30 * 60 * 1000,
+    retry: 1,
+    enabled: !!sdk,
+  });
+}
