6e6beac1d93b94d5ef106ccca56531505a73f7be
gkhulbe4 <khulbegarvit4@gmail.com>
2025-10-09 19:52:02 +0530

    minor

--- src/react/hooks/quizzes/useQuizTitles.ts
+++ src/react/hooks/quizzes/useQuizTitles.ts
@@ -3,22 +3,25 @@ import { useAppx } from "../../useAppx";
 import { QuizTitle } from "../../../types/quizTypes";
 
 const PAGE_SIZE = 10;
-export function useQuizTitles() {
+
+export function useQuizTitles(exam?: string) {
   const { sdk } = useAppx();
 
   return useInfiniteQuery<QuizTitle[], Error>({
-    queryKey: ["quizTitles"],
+    queryKey: ["quizTitles", exam],
     queryFn: async ({ pageParam = 0 }) => {
       if (!sdk) throw new Error("SDK not initialized");
 
-      const res = await sdk.quiz.getQuizTitles(pageParam!.toString());
+      const res = await sdk.quiz.getQuizTitles(
+        pageParam!.toString(),
+        exam && exam !== "All" ? exam : undefined
+      );
 
       return res.data ?? [];
     },
     getNextPageParam: (lastPage, allPages) => {
       if (!lastPage || lastPage.length < PAGE_SIZE) return undefined;
-      const nextStart = allPages.length * PAGE_SIZE;
-      return nextStart;
+      return allPages.length * PAGE_SIZE;
     },
     initialPageParam: 0,
     enabled: !!sdk,
